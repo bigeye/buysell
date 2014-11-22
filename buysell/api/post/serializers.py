55b5afe421b38f4cb43a533ff03adaeb7101e181
Horknox <horknox@gmail.com>
2014-11-23 05:22:15 +0900

    Update Post Serializer Handler

--- buysell/api/post/serializers.py
+++ buysell/api/post/serializers.py
@@ -21,11 +21,12 @@ class TagSerialzier(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
 
     images = PostImageSerializer(many=True, required=False)
-    writer = UserSerializer(required=True)
+    writer = UserSerializer(read_only=True)
 
     class Meta:
         model = Post
-        fields = ('id', 'writer', 'title', 'content', 'create_date', 'update_date', 'images')
+        fields = ('id', 'writer', 'title', 'content', 'is_private', 
+                'create_date', 'update_date', 'images')
 
     def restore_object(self, attrs, instance=None):
         if instance is not None:
@@ -35,24 +36,15 @@ class PostSerializer(serializers.ModelSerializer):
             instance.content = attrs.get('content', instance.content)
             return instance
 
-        instance = Post(writer = self.request['context'].user,
+        request = self.context.get('request', None)
+
+        is_private = False
+        if attrs.get('is_private', None) is not None:
+            is_private = True if attrs['is_private'] else False
+
+        instance = Post(writer = self.context['request'].user,
                 title = attrs['title'],
-                is_private = True if attrs['is_private'] == 'true' else False,
+                is_private = is_private,
                 content = attrs['content'])
 
         return instance
-
-class PostRetreiveSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Post
-        fields = ('id', 'writer', 'title', 'content', 'create_date', 'update_date', 'images')
-        # fields = ('writer', 'title', 'content', 'create_date', 'update_date', 'images')
-
-    # def restore_object(self, attrs, instance=None):
-    #     pass
-    # post_id = serializers.IntegerField()
-
-    # def validate(self, attrs):
-    #     post = Post.objects.get(id=attrs['post_id'])
-    #     return post
--- buysell/api/post/views.py
+++ buysell/api/post/views.py
@@ -1,19 +1,22 @@
+from django.http import Http404
+
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
 
-from buysell.api.post.serializers import PostSerializer, PostRetreiveSerializer
+from buysell.api.post.serializers import PostSerializer
 from buysell.api.post.models import Post
 
+class PostPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method == 'GET':
+            return True
+        return request.user and request.user.is_authenticated()
+
 class PostHandler(APIView):
-
-    class PostPermission(permissions.BasePermission):
-
-        def has_permission(self, request, view):
-            if request.method == 'GET':
-                return True
-            return request.user and request.user.is_authenticated()
 
     permission_classes = (PostPermission,)
 
@@ -24,41 +27,36 @@ class PostHandler(APIView):
             raise Http404
 
     def get(self, request, post_id=None, format=None):
-        serializer = PostRetreiveSerializer(get_object(post_id))
+        serializer = PostSerializer(self.get_object(post_id))
         return Response(serializer.data)
 
     def put(self, request, post_id=None, format=None):
 
-        data = { 'post_id' : post_id }
-        serializer = PostRetreiveSerializer(data=data)
+        post = self.get_object(post_id)
 
-        if serializer.is_valid():
-            p_serializer = PostSerializer(serializer.object, data=request.DATA, partial=True)
-            if p_serializer.is_valid():
-                p_serializer.save()
-                return Response(p_serializer.data, status=status.HTTP_200_OK)
+        p_serializer = PostSerializer(post, data=request.DATA, partial=True)
+        if p_serializer.is_valid():
+            p_serializer.save()
+            return Response(p_serializer.data, status=status.HTTP_200_OK)
 
         else:
             return Response(p_serializer.errors,
                     status=status.HTTP_400_BAD_REQUEST)
 
-class PostsHandler(APIView):
-    class PostPermission(permissions.BasePermission):
+class PostListHandler(ListAPIView):
 
-        def has_permission(self, request, view):
-            if request.method == 'GET':
-                return True
-            return request.user and request.user.is_authenticated()
+    serializer_class = PostSerializer
+    queryset = serializer_class.Meta.model.objects.all()
+    paginate_by = 20
+    paginate_by_param = 'page_size'
+    max_paginate_by_param = '100'
 
     permission_classes = (PostPermission,)
 
-    def get(self, request, format=None):
-        posts = Post.objects.all()
-        serializer = PostRetreiveSerializer(posts, many=True)
-        return Response(serializer.data)
-
     def post(self, request, post_id=None, format=None):
-        serializer = PostRetreiveSerializer(data=request.DATA)
+        serializer = PostSerializer(data=request.DATA, context={
+            'request' : request
+            })
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)

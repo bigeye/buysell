5719291ef490734158f23531a34760912c143a79
Horknox <horknox@gmail.com>
2014-11-23 03:09:09 +0900

    Adding post views.py

--- buysell/apps/post/views.py
+++ buysell/apps/post/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from buysell.apps.post.serializers import PostSerializer, PostRetreiveSerializer
+
+class PostHandler(APIView):
+
+    def get(self, request, post_id=None, format=None):
+
+        data = { 'post_id' : post_id }
+        serializer = PostRetreiveSerializer(data=data)
+
+        if serializer.is_valid():
+            p_serializer = PostSerializer(serializer.object)
+            return Response(p_serializer.data, status=status.HTTP_200_OK)
+
+        else:
+            return Response(p_serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, post_id=None, format=None):
+
+        data = { 'post_id' : post_id }
+        serializer = PostRetreiveSerializer(data=data)
+
+        if serializer.is_valid():
+            p_serializer = PostSerializer(serializer.object, data=request.DATA, partial=True)
+            if p_serializer.is_valid():
+                p_serializer.save()
+                return Response(p_serializer.data, status=status.HTTP_200_OK)
+
+        else:
+            return Response(p_serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST)

5826838b0a280d70eaff9a68e80188db26fa2676
SeongHyeok Im <ishez9210@gmail.com>
2014-11-23 05:58:17 +0900

    Modify urls.py and settings.py.

--- buysell/urls.py
+++ buysell/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'buysell.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'buysell.apps.home.views.index'),
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^api/', include('buysell.api.urls')),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

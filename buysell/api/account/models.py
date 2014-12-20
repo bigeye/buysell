55db095a57f655f246c3aec2174b25094f683a5e
Horknox <horknox@gmail.com>
2014-12-21 05:20:15 +0900

    Fix bugs on UserProfile accessing. Create UserProfileSerializer to have user profile serialization.

--- buysell/api/account/models.py
+++ buysell/api/account/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 from buysell.api.post.models import Tag, Post
 
 class UserProfile(models.Model):
     
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile', unique=True)
     subscribe_tag = models.ManyToManyField(Tag)
     profile_image = models.ImageField(upload_to = 'profile', default='default_profile.png')
     phone = models.CharField(max_length = 50)
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
 class OAuthToken(models.Model):
 
